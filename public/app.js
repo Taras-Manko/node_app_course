9ad61f6387035a023b209d916cf0544e5ac50904
Taras Man'ko <manko5613@gmail.com>
2021-02-04 23:07:14 +0300

    first

---
+++ public/app.js
@@ -0,0 +1,55 @@
+
+
+const toDate = date => {
+  return new Intl.DateTimeFormat('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }).format(new Date(date))
+}
+
+
+document.querySelectorAll('.date').forEach(node => {
+  node.textContent = toDate(node.textContent)
+})
+
+const $card = document.querySelector('#card')
+if ($card) {
+  $card.addEventListener('click', event => {
+    if (event.target.classList.contains('js-remove')) {
+      const id = event.target.dataset.id
+      const csrf = event.target.dataset.csrf
+      
+      fetch('/card/remove/' + id, {
+        method: 'delete',
+        headers: {
+          'X-XSRF-TOKEN': csrf
+        },
+      }).then(res => res.json())
+        .then(card => {
+          if (card.courses.length) {
+            const html = card.courses.map(c => {
+              return `
+              <tr>
+                <td>${c.title}</td>
+                <td>${c.count}</td>
+                <td>
+                  <button class="btn btm-small js-remove" data-id="${c.id}">Удалить</button>
+                </td>
+              </tr>
+              `
+            }).join('')
+            $card.querySelector('tbody').innerHTML = html
+          } else {
+            $card.innerHTML = '<p>Корзина пуста</p>'
+          }
+        })
+    }
+    
+  })
+} 
+
+M.Tabs.init(document.querySelectorAll('.tabs'))
